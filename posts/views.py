d780add617a8fd9413c41ececc2636289fc4689d
Marina <marinella2012@gmail.com>
2021-04-19 22:22:30 +0400

    some fixes

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,4 @@
-from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
--- yatube/settings.py
+++ yatube/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
